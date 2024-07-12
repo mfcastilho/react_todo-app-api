00ce647b1298a128aa0fffd80c9769c657efc75e
mfcastilho <fredericocastilho@hotmail.com>
2024-07-11 22:54:16 -0300

    feat: GetAllTodos finalized

--- src/controllers/GetAllTodosController.ts
+++ src/controllers/GetAllTodosController.ts
@@ -1,12 +1,15 @@
-
+import { Filters } from '@/types/Filters';
 import { GetAllTodosService } from '../services';
-import {Request, Response } from 'express';
+import { Request, Response } from 'express';
+
 
 class GetAllTodosController {
   async handle(req: Request, res: Response) {
+    const { userId, completed }: Filters = req.query;
+
     const service = new GetAllTodosService();
 
-    const todos = await service.execute();
+    const todos = await service.execute(userId, completed);
 
     return res.status(200).json(todos);
   }
--- src/services/GetAllTodosService.ts
+++ src/services/GetAllTodosService.ts
@@ -2,9 +2,25 @@ import { Todo } from "../entities/Todo";
 import TodoRepository from "../repositories/todo.repository";
 
 
+
 export class GetAllTodosService {
-  async execute(): Promise<Todo[]> {
-    const todos = await TodoRepository.findMany();
+  async execute(userId?: string, completed?: string): Promise<Todo[]> {
+    const filters: { userId?: number, completed?: boolean } = {};
+
+    if (userId) {
+      filters.userId = Number(userId);
+    }
+
+    if (completed) {
+      filters.completed = completed === 'string';
+    }
+
+    const todos = await TodoRepository.findMany({
+      where: {
+        userId: filters.userId,
+        completed: filters.completed ?? undefined,
+      }
+    });
 
     return todos;
   }
